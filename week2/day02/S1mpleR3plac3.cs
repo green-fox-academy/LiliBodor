1c87da50f1c6d9cdb4ddb17221855dba95f2f19b
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 14:33:34 +0200

    simple replace commit

---
+++ week2/day02/S1mpleR3plac3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleReplace
+{
+	public class SimpleReplace
+	{
+		public static void Main(string[] args)
+		{
+			string example = "In a dishwasher far far away";
+
+			example = example.Replace("dishwasher", "galaxy");
+
+
+			Console.WriteLine(example);
+		}
+	}
+}
+
