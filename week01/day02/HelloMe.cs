125c1546a437acff13b6d0ace94ab1e6b86710b2
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 13:13:15 +0200

    hello commit

---
+++ week01/day02/HelloMe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloMe
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Lili!");
+        }
+    }
+}
