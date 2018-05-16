549602f992fe49de5efc4b5a46563bc2d4d4b1ba
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 14:43:09 +0200

    TodoPr1nt commitum

---
+++ week2/day02/TodoPrint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace TodoPrint
+{
+    public class TodoPrint
+    {
+        public static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+
+			StringBuilder todoTextBuilder = new StringBuilder(todoText);
+
+			todoTextBuilder.Insert(0, "My todo:\n");
+			todoTextBuilder.Append(" - Download games");
+			todoTextBuilder.Append("\n\t- Diablo");
+            
+
+			Console.WriteLine(todoTextBuilder);
+        }
+    }
+}
