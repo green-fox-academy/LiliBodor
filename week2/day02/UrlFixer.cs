178ca93d3b859462a0b96be383fe2ca20d47f334
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 14:14:17 +0200

    url commit

---
+++ week2/day02/UrlFixer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UrlFixer
+{
+    public class UrlFixer
+    {
+        public static void Main(string[] args)
+        {
+            string url = "https//www.reddit.com/r/nevertellmethebots";
+			url = url.Replace("bots", "odds");
+
+			url = url.Replace("//", "://");
+           
+           
+
+            Console.WriteLine(url);
+        }
+    }
+}
