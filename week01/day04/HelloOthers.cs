585ce31ed303009ed3e40ec39f7f0668ce32dd4c
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 13:32:31 +0200

    helloothers commit

---
+++ week01/day04/HelloOthers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Hello Mate1!");
+			Console.WriteLine("Hello Mate2!");
+			Console.WriteLine("Hello Mate3!");
+        }
+    }
+}
