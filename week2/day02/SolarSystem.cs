91347412950ebbac0074b6f05af3ffc9c64deb2a
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 15:07:17 +0200

    solar syssstem commit

---
+++ week2/day02/SolarSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarSystem
+{
+    public class SolarSystem
+    {
+        public static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune" };
+
+
+            Console.WriteLine(PutSaturn(planetList));
+
+        }
+        public static string PutSaturn(List<string> planetList)
+        {
+            planetList.Insert(5, "Saturn");
+
+            return string.Join(", ", planetList);
+        }
+
+    }
+}
