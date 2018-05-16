93e4e460a8361f8491a49ca220517e886ebaabf6
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 15:24:40 +0200

    manipulation commit

---
+++ week2/day01/StringManipuation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SumElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] r = { 54, 23, 66, 12 };
+            
+
+			Console.WriteLine(r[1] += r[2]);
+
+           
+            Console.ReadLine();
+        }
+    }
+}
