4ec4a0d2fd458509ac6943b54a697f143412b21d
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 15:32:14 +0200

    double1tems commit

---
+++ week2/day01/DoubleItems.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] numList = { 3, 4, 5, 6, 7 };
+
+			for (int i = 0; i < numList.Length; i++)
+			{
+				Console.WriteLine(numList[i] *= 2);
+			}
+            Console.ReadLine();
+        }
+    }
+}
