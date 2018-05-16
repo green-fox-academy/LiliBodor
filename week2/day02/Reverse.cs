c5e1d4ae5cc9c749c857936517cbbb001606fc2c
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 14:39:06 +0200

    R3v3rs3 comm1t

---
+++ week2/day02/Reverse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Reverse
+{
+	public class Reverse
+	{
+		public static void Main(string[] args)
+		{
+			string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+			Console.WriteLine(Reversed(reversed));
+
+
+		}
+
+		static string Reversed(string reversedReverese)
+		{
+			string reserve = "";
+
+			for (int i = reversedReverese.Length - 1; i >= 0; i--)
+			{
+				reserve += reversedReverese[i];
+			}
+			return  reserve;
+		
+		
+			}
+		}
+	}
+       
