070146cea1e7f5387e54b4d401eae2515df205e9
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 15:43:43 +0200

    sumAll commit

---
+++ week2/day01/SumAll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] ai = { 3, 4, 5, 6, 7 };
+
+			Console.WriteLine(ai[1] + ai[2] + ai[3]);
+			// - Print the sum of the elements in `ai`
+            Console.ReadLine();
+        }
+    }
+}
