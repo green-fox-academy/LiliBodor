5486359bb700c09ec0a8443fb61cce303d547774
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 15:27:15 +0200

    changing element commit

---
+++ week2/day01/ChangeElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ChangeElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int[] s = { 1, 2, 3, 8, 5, 6 };
+
+			int newS = 4;
+
+			s[3] = newS;
+
+			Console.WriteLine(s[3]);
+				
+          
+            Console.ReadLine();
+        }
+    }
+}
