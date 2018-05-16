b1ce26e28905a9ad36dc595579b7750891f76e2b
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-16 14:18:01 +0200

    takes so long

---
+++ week2/day02/TakesLonger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace TakesLonger
+{
+    public class TakesLonger
+    {
+        public static void Main(string[] args)
+        {
+            string quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+
+			StringBuilder quoteBuiler = new StringBuilder(quote);
+
+			quoteBuiler.Insert(21, "always takes longer than ");
+
+			Console.WriteLine(quoteBuiler);
+        }
+    }
+}
