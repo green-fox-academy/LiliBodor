1a85500e3061aa154f1b4b31af1488d4a543cc45
Lili Bodor <lili.bodor.7@gmail.com>
2018-05-25 12:05:01 +0200

    chore(bunny)

---
+++ week3/Day 04/Bunny/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Bunny
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfBunnies;
+
+
+            Console.WriteLine("Number of bunnies: ");
+            numberOfBunnies = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number of ears: ");
+			//earsOfTheBunnies = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(BunnoBunny(numberOfBunnies));
+
+        }
+        public static int BunnoBunny(int numberOfBunnies)
+        {
+			if (numberOfBunnies == 1)
+            {
+                return 2;
+            }
+            else
+            {
+				return 2 + BunnoBunny(numberOfBunnies - 1);
+            }
+        }
+    }
+}
---
+++ week3/Day03/RecursionCounter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RecursionCounter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            n = 8;
+			Console.WriteLine(Recursion(n));
+
+        }
+
+        public static int Recursion(int n)
+        {
+            
+			if (n == 1)
+			{
+				return 1;
+
+			}
+            
+			else
+			{
+				Console.WriteLine(n);
+                return Recursion(n - 1);
+
+
+            }
+        }
+    }
+}
